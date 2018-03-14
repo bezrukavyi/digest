5043a3ecddbd7d3854a75fed025504177e243222
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2018-03-14 19:46:28 +0200

    Added devise token auth | Replace dashboard to api

--- app/controllers/api/v1/dashboard/subscriptions_controller.rb
+++ app/controllers/api/v1/dashboard/subscriptions_controller.rb
@@ -1,5 +1,5 @@
-module Dashboard
-  class SubscriptionsController < ApplicationController
+module Api::V1::Dashboard
+  class SubscriptionsController < Api::V1::ApplicationController
     def index
       @result = Subscriptions::Index.call(params: params, current_user: current_user)
       respond_to_result
