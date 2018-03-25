79ca1b235f70aac4ab531286e1d6d223d75be080
Yaroslav Bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2018-03-25 16:28:56 +0300

    Add condition (#44)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  if Rails.env.development? # rubocop:disable Style/IfUnlessModifier
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
